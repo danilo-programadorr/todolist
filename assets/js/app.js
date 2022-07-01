145161ae99401293ddc39e35e6fc4959a579b5de
danilo-programadorr <danilo.o.santos@hotmail.com>
2022-07-01 06:59:13 -0300

    adicionando funções nos elementos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,65 @@
 'use strict';
 
-/* <div class="todo-item">
-<div class="trash"></div>
-<div class="edit"></div>
-<input type="checkbox" class="input">
-<label class="content-tarefa" for="input">tarefa</label><br>
-<div class="line"></div>
-<label class="description" for="input">descrição</label>
-</div> */
 
-const createMyList = (tarefa,description) => {
+let banco = [{
+    'tarefa': 'Estudando','status': '' 
+  },
+  {
+    'tarefa': 'jogar','status': ''
+  }
+
+]
+// const inputForm = document.querySelectorAll('.input-form')
+
+// const inputForms = document.querySelector('.input-form')
+// const input1 = inputForms[0]
+// const input2 = inputForms[1]
+
+const createMyList = (tarefa,indice,status) => {
+//  const inputTarefa = tarefa.input1
+//  const inputDescription = description.input2
+//  console.log(inputTarefa,inputDescription)
   const item = document.createElement('div')
   item.classList.add('todo-item')
   item.innerHTML = `
-      <div class="trash"></div>
-      <div class="edit"></div>
-      <input type="checkbox" class="input">
-      <label class="content-tarefa" for="input">${tarefa}</label><br>
-      <div class="line"></div>
-      <label class="description" for="input">${description}</label>
+  <div class="trash" data-indice=${indice}></div >
+  <div class="edit"></div>
+  <input type="checkbox" class="input" ${status} data-indice=${indice}>
+  <label class="content-tarefa" for="input">${tarefa}</label><br>
+  <div class="line"></div>
   `
- document.querySelector('.todo-list').appendChild(item)
+  // <label class="description" for="input">${description}</label>
+  document.querySelector('.todo-list').appendChild(item)
 }
+
+const limparTarefas = () => {
+  const todoList = document.querySelector('.todo-list')
+  while (todoList.firstChild) {
+    todoList.removeChild(todoList.lastChild)
+  }
+}
+const renderScreen = () => {
+  limparTarefas()
+  banco.forEach((item,indice) => createMyList(item.tarefa, item.status, indice))
+}
+const inserirItem = (event) => {
+  let tecla = event.key
+  let text = event.target.value
+  if (tecla === 'Enter') {
+    banco.push({
+      'tarefa': text
+    })
+    renderScreen()
+    event.target.value = ''
+  }
+}
+
+const clickItem = (event) => {
+  const element = event.target
+  console.log(element)
+}
+
+document.addEventListener('click',clickItem)
+document.querySelector('.input-form').addEventListener('keypress', inserirItem)
+
+renderScreen()
--- assets/js/dist/app.dev.js
+++ assets/js/dist/app.dev.js
@@ -1,16 +1,60 @@
 'use strict';
-/* <div class="todo-item">
-<div class="trash"></div>
-<div class="edit"></div>
-<input type="checkbox" class="input">
-<label class="content-tarefa" for="input">tarefa</label><br>
-<div class="line"></div>
-<label class="description" for="input">descrição</label>
-</div> */
 
-var createMyList = function createMyList(tarefa, description) {
+var banco = [{
+  'tarefa': 'Estudando',
+  'status': ''
+}, {
+  'tarefa': 'jogar',
+  'status': ''
+}]; // const inputForm = document.querySelectorAll('.input-form')
+// const inputForms = document.querySelector('.input-form')
+// const input1 = inputForms[0]
+// const input2 = inputForms[1]
+
+var createMyList = function createMyList(tarefa, indice, status) {
+  //  const inputTarefa = tarefa.input1
+  //  const inputDescription = description.input2
+  //  console.log(inputTarefa,inputDescription)
   var item = document.createElement('div');
   item.classList.add('todo-item');
-  item.innerHTML = "\n      <div class=\"trash\"></div>\n      <div class=\"edit\"></div>\n      <input type=\"checkbox\" class=\"input\">\n      <label class=\"content-tarefa\" for=\"input\">".concat(tarefa, "</label><br>\n      <div class=\"line\"></div>\n      <label class=\"description\" for=\"input\">").concat(description, "</label>\n  ");
+  item.innerHTML = "\n  <div class=\"trash\" data-indice=".concat(indice, "></div >\n  <div class=\"edit\"></div>\n  <input type=\"checkbox\" class=\"input\" ").concat(status, " data-indice=").concat(indice, ">\n  <label class=\"content-tarefa\" for=\"input\">").concat(tarefa, "</label><br>\n  <div class=\"line\"></div>\n  "); // <label class="description" for="input">${description}</label>
+
   document.querySelector('.todo-list').appendChild(item);
 };
+
+var limparTarefas = function limparTarefas() {
+  var todoList = document.querySelector('.todo-list');
+
+  while (todoList.firstChild) {
+    todoList.removeChild(todoList.lastChild);
+  }
+};
+
+var renderScreen = function renderScreen() {
+  limparTarefas();
+  banco.forEach(function (item, indice) {
+    return createMyList(item.tarefa, item.status, indice);
+  });
+};
+
+var inserirItem = function inserirItem(event) {
+  var tecla = event.key;
+  var text = event.target.value;
+
+  if (tecla === 'Enter') {
+    banco.push({
+      'tarefa': text
+    });
+    renderScreen();
+    event.target.value = '';
+  }
+};
+
+var clickItem = function clickItem(event) {
+  var element = event.target;
+  console.log(element);
+};
+
+document.addEventListener('click', clickItem);
+document.querySelector('.input-form').addEventListener('keypress', inserirItem);
+renderScreen();
